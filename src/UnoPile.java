d43ee00633591fce2158a4c971df955c4f22abcd
YashTotale <totaleyash@gmail.com>
2020-11-30 09:45:00 -0800

    UnoPile set up

--- src/UnoPile.java
+++ src/UnoPile.java
@@ -1,25 +1,50 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class UnoPile {
+	private ArrayList<UnoCard> pile;
 
+	public UnoPile() {
+		this.pile = new ArrayList();
+	}
 	
 	public void draw(Graphics g) {
 
 	}
 
 	public int size() {
-		
-		return -1;
+		return this.pile.size();
 	}
+
 	public UnoCard deal() {
-		
-		return null;
+		return this.pile.remove(this.pile.size() - 1);
 	}
-	public void add(UnoCard unoCard) {
-		
+
+	public void add(@NotNull UnoCard unoCard) {
+		String color = unoCard.getColor();
+		int rank = unoCard.getRank();
+
+		for (int i = 0; i < this.pile.size(); i++) {
+			UnoCard current = this.pile.get(i);
+
+			if(color.equals(current.getColor())) {
+				if(rank < current.getRank()) {
+					this.pile.add(i, unoCard);
+					return;
+				} if(i != this.pile.size() -1 && !this.pile.get(i + 1).equals(color)) {
+					this.pile.add(i + 1, unoCard);
+					return;
+				}
+			}
+		}
+
+		this.pile.add(unoCard);
 	}
 
 	public void shuffle() {
-		
+		Collections.shuffle(pile);
 	}
 }
